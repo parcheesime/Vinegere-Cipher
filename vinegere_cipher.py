94b3430b435a0b15a3d53108ac0119619787c8e8
Aletia Trepte <aletia@zoho.com>
2022-05-01 20:01:09 -0700

    Keyword generator function

--- vinegere_cipher.py
+++ vinegere_cipher.py
@@ -1,12 +1,13 @@
 # Vigenere Cipher
 
 # Translate some text into ciphertext, each letter is represented by a numbered index
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z']
 
 
 def text_to_num(text):
     cipher = []
+    text = text.lower()
     for p in text:
         for l in alphabet:
             if p == l:
@@ -15,27 +16,24 @@ def text_to_num(text):
 
 
 def num_to_text(nums):
-    plain = []
     for c in nums:
-        for i in alphabet:
-            if c == alphabet.index(i):
-                plain.append(i)
+       plain = [i for i in alphabet if c == alphabet.index(i)]
     return plain
 
 
 def make_key(plaintext, keyword):
     add_on = len(plaintext) - len(keyword)
     key_list = text_to_num(keyword)
-    print(len(plaintext))
-    for k in range(0, add_on):
+    for k in range(add_on):
         key_list.append(key_list[k])
-    print(len(key_list))
     return key_list
 
 
 def encrypt(plaintext, keyword):
     pt = text_to_num(plaintext)
-    kw = text_to_num(keyword)
+    kw = make_key(plaintext, keyword)
+    encryption = [str((pt[i] + kw[i]) % 26) for i in range(len(pt))]
+    return "".join(encryption)
 
 
 def decrypt(cyphertext, keyword):
@@ -44,10 +42,11 @@ def decrypt(cyphertext, keyword):
 
 ask = input("Encrypt(E) or Decrypt(D) a message?")
 if ask.upper() == "E":
-    plaintext = input('Enter the plaintext message to be encrypted: ')
-    keyword = input("Enter the keyword for the message: ")
-    print(text_to_num(plaintext))
-    print(make_key(plaintext, keyword))
+    plain_text = input('Enter the plaintext message to be encrypted: ').replace(" ", "")
+    key_word = input("Enter the keyword for the message: ").replace(" ", "")
+    print("Length of message: {}".format(len(text_to_num(plain_text))))
+    print("Length of key: {}".format(len(make_key(plain_text, key_word))))
+    print(encrypt(plain_text, key_word))
 elif ask.upper() == "D":
     cyphertext = input("Enter the encrypted message: ")
     keyword = input("Enter the keyword for the message: ")
